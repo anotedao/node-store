ffe731bbd2deb856cd5242e132a2fcdd417f2440
Pragmatic <cryptopragmatic@protonmail.com>
2023-12-10 04:30:47 +0100

    store work

--- bsc.go
+++ bsc.go
@@ -182,7 +182,7 @@ func DecodeTransactionInputData(contractABI *abi.ABI, data []byte) string {
 	}
 
 	if method.Name == "mintNode" {
-		addr = inputsMap["address"].(string)
+		addr = inputsMap["_domain"].(string)
 	}
 
 	return addr

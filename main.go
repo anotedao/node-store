a14fd2f1f5b507e91a8ba3e9f7bbe995cd5b3b1d
Pragmatic <cryptopragmatic@protonmail.com>
2023-12-16 16:34:08 +0100

    price increase

--- main.go
+++ main.go
@@ -24,5 +24,11 @@ func main() {
 
 	anc = initAnote()
 
+	price := int64(5)
+	dataTransaction("%s__nodePrice", nil, &price, nil)
+
+	tier := int64(10)
+	dataTransaction("%s__nodeTier", nil, &tier, nil)
+
 	initBsc()
 }
